ebd2c5bf0ad3f14ae275e763eb6af5eda9600a9f
Pavel Astral <astrasourcenext@gmail.com>
2021-11-19 16:42:16 +0700

    Revert "Revert "RK methodДобавьте файлы проекта.""
    This reverts commit 3643d3e6665193a2c5ac637c8e3ecbe9dbfad3d5.

---
+++ Lab6/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using static Lab6.DiffEq;
+
+namespace Lab6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DiffEqSolveSettings settings = new() {
+                a = 0, b = 0.5, n = 10, initValue = 0,
+                eqList = new List<DiffEqDelegate> {F1FirstDev},
+                order = 1, solveOrder = 1
+            };
+
+            var res = DiffEqSolve.RK(settings);
+        }
+    }
+}
